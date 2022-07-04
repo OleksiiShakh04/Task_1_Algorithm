94884c940b32f182e2d69e2af2a57d840abd6864
OleksiiShakh04 <oleksii.shakh04@gmail.com>
2022-07-04 16:11:19 +0300

    First
    ...

---
+++ TasksForTrainee/TasksForTrainee/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace TasksForTrainee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+             bool checkWorkAlgoritm = false;
+            
+           // two arrays for check algoritm
+         int[,] Array9x9 = new int[9, 9]
+         {
+                {7,8,4, 1,5,9, 3,2,6 },
+                {5,3,9, 6,7,2, 8,4,1},
+                {6,1,2, 4,3,8, 7,5,9},
+
+                {9,2,8, 7,1,5, 4,6,3},
+                {3,5,7, 8,4,6, 1,9,2},
+                {4,6,1, 9,2,3, 5,8,7},
+
+                {8,7,6, 3,9,4, 2,1,5},
+                {2,4,3, 5,6,1, 9,7,8},
+                {1,9,5, 2,8,7, 6,3,4}
+         };
+
+         int[,] Array4x4 = new int[4, 4]
+          {
+                {7,8,    4,1},
+                {5,3,    9,6},
+
+                {6,1,    2,4},
+                {9,2,    8,7},
+
+          };
+
+            Console.WriteLine("Enter  N:");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n == 9) { checkWorkAlgoritm = Sudoku.AlgoritmSudoku(n, Array9x9); Console.WriteLine( "Field 9x9: " + checkWorkAlgoritm); }
+            else if (n == 4 ) { checkWorkAlgoritm = Sudoku.AlgoritmSudoku(n, Array4x4); Console.WriteLine("Field 4x4: " + checkWorkAlgoritm); }
+           
+
+        }
+    }
+}
---
+++ TasksForTrainee/TasksForTrainee/Sudoku.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksForTrainee
+{
+    class Sudoku
+    {
+    
+      public static  bool AlgoritmSudoku(int n, int[,] field)
+        {
+
+            double NSqrt = Math.Sqrt(n);
+            int[] array_part = new int[n];
+
+            if (n > 1 && n % NSqrt == 0 && field.GetLength(0) == n && field.GetLength(1) == n)
+            {
+
+                // 1 - check all rows horizontal
+                for (int i = 0; i < field.GetLength(0); i++)
+                {
+                    for (int j = 0; j < field.GetLength(1); j++)
+                    {
+                        array_part[j] = field[i,j];
+                    }
+                    if (!CheckOnePart(array_part)) { Console.WriteLine("1 step"); return false; };
+                }
+
+
+                // 2 - check all rows vertical
+
+                for (int i = 0; i < field.GetLength(0); i++)
+                {
+                    for (int j = 0; j < field.GetLength(1); j++)   
+                    {
+                        array_part[j] = field[j, i];
+                    }
+                    if (!CheckOnePart(array_part)) { Console.WriteLine("2 step"); return false; };
+                }
+
+
+                // 3 - check  all parts , example 3x3, 2x2... 
+                for (int i = 0; i < n; i += Convert.ToInt32(NSqrt))
+                {
+                    for (int j = 0; j < n; j += Convert.ToInt32(NSqrt))
+                    {
+                        int index = 0;
+                        for (int s = 0; s < Math.Sqrt(n); s++)
+                        {
+                            for (int q = 0; q < Math.Sqrt(n); q++)
+                            {
+                                array_part[index] = field[i + s, j + q]; index++;
+                            }
+                        }
+                        if (!CheckOnePart(array_part)) { Console.WriteLine("3 step"); return false; };
+                    }
+
+                }
+
+                return true;
+            }
+            else return false;
+
+           
+        }
+
+
+       public static bool CheckOnePart(int[] array_part)
+        {
+            for(int i = 0; i < array_part.Length; i++)
+            {
+                for (int j = 0; j < array_part.Length; j++)
+                {
+                    if(array_part[i] == array_part[j] && i!=j) { return false; }
+                }
+
+            }
+
+            return true;
+        }
+
+    }
+
+}
